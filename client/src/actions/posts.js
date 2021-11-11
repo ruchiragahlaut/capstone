9673cc6e559a41cfd45e2a85305c3568ba7515a9
Ruchira Gahlaut <ruchiragahlaut@gmail.com>
2021-11-11 12:44:34 +0530

    commit

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -10,7 +10,20 @@ export const getPosts=()=> async(dispatch) =>{
         dispatch({type:'FETCH_ALL', payload:data});
     }
     catch(error){
-        console.log(error.message);
+        console.log(error.message+"hello");
     }
 }
+
+export const createPost=(post)=> async(dispatch) =>{
+
+    try{
+        const {data} =await api.createPost(post);
+        dispatch({type:'CREATE', payload:data});
+    }
+    catch(error){
+        console.log(error.message+"hello");
+    }
+}
+
+
 
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
-const url = 'hhtp://localhost:5000/posts';
+const url = 'http://localhost:5000/posts';
 export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url,newPost);
 
