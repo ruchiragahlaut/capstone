b9a7702300dc80e757db4038ccd2d8cb02a40980
Ruchira Gahlaut <ruchiragahlaut@gmail.com>
2021-11-12 00:02:11 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,8 @@ const App = () =>{
     return(
         <Container maxWidth = 'lg'>
             <AppBar className = {classes.appBar} position="static" color="secondary">
-                <Typography className={classes.heading} variant ="h2" align ="center">Memories</Typography>
-                <img className={classes.image} src ={memories} alt="memories" height="60" />
+                <Typography className={classes.heading} variant ="h2" align ="center">BLOGNET</Typography>
+               
             </AppBar>
             
             <Grow in> 
